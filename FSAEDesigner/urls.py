d9bc63303a2d6200382b5e65005b8be3a5eca462
kartGRAM <ahuaya1024@gmail.com>
2022-04-11 00:19:33 +0900

    verifyを追加

--- FSAEDesigner/urls.py
+++ FSAEDesigner/urls.py
@@ -3,10 +3,12 @@ from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 app_name = 'FSAEDesigner'
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
