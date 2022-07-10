574ec761d1fb84e0637af42b76a883e334a45623
kartGRAM <ahuaya1024@gmail.com>
2022-07-10 14:36:09 +0900

    saveAsを追加

--- FSAEDesigner/serializer.py
+++ FSAEDesigner/serializer.py
@@ -3,10 +3,19 @@ from rest_framework import serializers
 
 
 class GeometryDesignerFileSerializer(serializers.ModelSerializer):
+    overwrite = serializers.BooleanField(required=True, write_only=True)
+    clientLastUpdated = serializers.DateTimeField(required=True, write_only=True)
+
     class Meta:
         model = GeometryDesignerFile
-        fields = ('name', 'user', 'is_public', 'thumbnail', 'content')
-        read_only_fields = ('username',)
+        fields = ('id','name','note', 'user', 'is_public','thumbnail',
+         'content',  "lastUpdated", "overwrite", "clientLastUpdated")
+        read_only_fields = ('created',)
         extra_kwargs = {
-            'password': {'write_only': True},
+            'overwrite': {'write_only': True},
+            'clientLastUpdated': {'write_only': True},
         }
+
+    def create(self, validated_data):
+        del validated_data["overwrite"], validated_data["clientLastUpdated"]
+        return super().create(validated_data)
--- FSAEDesigner/views_drf.py
+++ FSAEDesigner/views_drf.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from .serializer import GeometryDesignerFileSerializer as gdFileSerializer
 from .models import GeometryDesignerFile as gdFile
+from django.utils import timezone
 
 @api_view(['POST'])
 def checkLoggedIn(request, format=None):
@@ -17,12 +18,40 @@ def checkLoggedIn(request, format=None):
 
 @api_view(['POST'])
 def gdSaveAs(request, format=None):
-    if request.method == "POST":
-        serializer = gdFileSerializer(data=request.data)
+    if request.method == "POST" and "name" in request.data:
+        data = request.data
+        data["user"] = request.user.id
+        data["lastUpdated"] = timezone.now()
+        serializer = gdFileSerializer(data=data)
         if serializer.is_valid():
+            data = serializer.validated_data
+            duplicated = list(gdFile.objects.filter(
+                user=request.user, name=data["name"]
+                ))
+            if len(duplicated) > 0:
+                if not data["overwrite"]:
+                    return Response(
+                        {"error": "File already exists."}
+                        , status=status.HTTP_409_CONFLICT)
+
+                duplicated = duplicated[0]
+                data = {k:v for k, v in data.items()}
+                del data['user']
+                serializer = gdFileSerializer(instance=duplicated,
+                  data=data,
+                  partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response({}, status=status.HTTP_202_ACCEPTED)
+                print(serializer.errors)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 @api_view(['GET'])
